c3c81346fc297f001388744607e4d6499c8e4a58
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2014-04-24 23:20:10 +0200

    Second commit
    Updated main window, way how the program is started, finished the Config
    Window and created a structure for the tests.xml file.

--- CCExtractorTester/Program.cs
+++ CCExtractorTester/Program.cs
@@ -8,44 +8,19 @@ namespace CCExtractorTester
 	{
 		public static void Main (string[] args)
 		{
-			bool console = args.Length > 0;
-			if (console) {
-				try {
-					VerifyAppConfig ();
-
-					// TODO: run program;
-				} catch (InvalidProgramException e) {
-					Console.WriteLine (e.Message);
+			if (args.Length > 0) {
+				if (!ConfigWindow.IsAppConfigOK ()) {
+					Console.WriteLine("Please edit the app.config file and try again");
+					return;
 				}
+				// TODO: check arguments.
+				// TODO: run program
 			} else {
 				Application.Init ();
-				try {
-					VerifyAppConfig ();
-				} catch (InvalidProgramException) {
-					RunSetUpScreen ();
-				}
 				MainWindow win = new MainWindow ();
 				win.Show ();
 				Application.Run ();
 			}
-		}
-
-		static void VerifyAppConfig ()
-		{
-			if (String.IsNullOrEmpty (ConfigurationManager.AppSettings ["ReportFolder"]) ||
-			   String.IsNullOrEmpty (ConfigurationManager.AppSettings ["SampleFolder"]) ||
-			   String.IsNullOrEmpty (ConfigurationManager.AppSettings ["CorrectResultFolder"]) ||
-			   String.IsNullOrEmpty (ConfigurationManager.AppSettings ["CCExtractorLocation"])) 
-			{
-				throw new InvalidProgramException ("Please edit the app.config file and try again");
-			}
-		}
-
-		static void RunSetUpScreen ()
-		{
-			ConfigWindow cw = new ConfigWindow ();
-			cw.Show ();
-			Application.Run ();
 		}
 	}
 }
