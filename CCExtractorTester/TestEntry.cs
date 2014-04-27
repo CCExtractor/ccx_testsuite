d6b49d73596897f1693b1bab81fb6e0f8240eb0a
wforums <webmaster@wforums.net>
2014-04-28 00:08:22 +0200

    Tester
    Started working on the Tester, which will be the main component of the
    program.

---
+++ CCExtractorTester/TestEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CCExtractorTester
+{
+	public class TestEntry
+	{
+		public String TestFile { get; private set; }
+		public String Command { get; private set; }
+		public String ResultFile { get; private set; }
+
+		public TestEntry (string testFile, string command, string resultFile)
+		{
+			TestFile = testFile;
+			Command = command;
+			ResultFile = resultFile;
+		}
+	}
+}
---
+++ CCExtractorTester/Tester.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Collections.Generic;
+
+namespace CCExtractorTester
+{
+	public class Tester
+	{
+		private List<TestEntry> Entries { get; set; } 
+
+		public Tester (string xml)
+		{
+			Entries = new List<TestEntry> ();
+			loadAndParseXML (xml);
+		}
+
+		void loadAndParseXML (string xml)
+		{
+			if (File.Exists (xml)) {
+				// TODO: create xml validation (xsd) and validate against it first.
+				XmlDocument doc = new XmlDocument ();
+				doc.Load (xml);
+				foreach (XmlNode node in doc.SelectNodes("//test")) {
+					XmlNode sampleFile = node.SelectSingleNode ("sample");
+					XmlNode command = node.SelectSingleNode ("cmd");
+					XmlNode resultFile = node.SelectSingleNode ("result");
+					Entries.Add(new TestEntry(sampleFile.Value,command.Value,resultFile.Value));
+				}
+			}
+			throw new InvalidDataException ("File does not exist");
+		}
+
+		public void RunTests(){
+			// TODO: finish
+		}
+	}
+}
+
