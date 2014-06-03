f7c471df13994ca679de72bf9cf2fa30ee402e60
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2014-06-04 00:24:50 +0200

    Comments, cleaning
    Comments in code + cleaning out some test code.

--- CCExtractorTester/Loggers/FileLogger.cs
+++ CCExtractorTester/Loggers/FileLogger.cs
@@ -3,12 +3,25 @@ using System.IO;
 
 namespace CCExtractorTester
 {
+	/// <summary>
+	/// A class that logs to a file.
+	/// </summary>
 	public class FileLogger : ILogger
 	{
+		/// <summary>
+		/// Gets or sets the an instance of StreamWriter.
+		/// </summary>
+		/// <value>The writer.</value>
 		StreamWriter Writer { get; set; }
+		/// <summary>
+		/// Gets or sets a value indicating whether this instance logging debug messages.
+		/// </summary>
+		/// <value><c>true</c> if this instance will print debug messages; otherwise, <c>false</c>.</value>
 		bool IsDebug { get; set; }
 
-
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CCExtractorTester.FileLogger"/> class.
+		/// </summary>
 		public FileLogger ()
 		{
 			Writer = File.CreateText ("Log-Run-"+DateTime.Now.ToFileTime()+".txt");
@@ -16,37 +29,55 @@ namespace CCExtractorTester
 		}
 
 		#region Ilogger implementation
-
+		/// <summary>
+		/// Activates the debug mode. If it's not activated, no debug messages will be logged.
+		/// </summary>
 		public void ActivateDebug ()
 		{
 			IsDebug = true;
 		}
-
+		/// <summary>
+		/// Logs a specific message on the Info level.
+		/// </summary>
+		/// <param name="message">A message to log.</param>
 		public virtual void Info (string message)
 		{
 			Writer.WriteLine ("[INFO] " + message);
 			Writer.Flush ();
 		}
-
+		/// <summary>
+		/// Logs a specific message on the Warn level.
+		/// </summary>
+		/// <param name="message">A message to log.</param>
 		public virtual void Warn (string message)
 		{
 			Writer.WriteLine ("[WARNING] " + message);
 			Writer.Flush ();
 		}
-
+		/// <summary>
+		/// Logs a specific message on the Error level.
+		/// </summary>
+		/// <param name="message">A message to log.</param>
 		public virtual void Error (string message)
 		{
 			Writer.WriteLine ("[ERROR] " + message);
 			Writer.Flush ();
 		}
-
+		/// <summary>
+		/// Logs a specific message on the Error level.
+		/// </summary>
+		/// <param name="message">A message to log.</param>
+		/// <param name="e">E.</param>
 		public virtual void Error (Exception e)
 		{
 			Error (e.Message);
 			Error ("Stacktrace:");
 			Error (e.StackTrace);
 		}
-
+		/// <summary>
+		/// Logs a specific message on the Debug level.
+		/// </summary>
+		/// <param name="message">A message to log.</param>
 		public virtual void Debug (string message)
 		{
 			if (IsDebug) {
@@ -54,7 +85,6 @@ namespace CCExtractorTester
 				Writer.Flush ();
 			}
 		}
-
 		#endregion
 	}
 }
