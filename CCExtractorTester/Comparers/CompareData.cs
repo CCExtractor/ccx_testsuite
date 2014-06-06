1a0f4c59e7ec7d1b0bb951592378e97f374386ca
wforums <webmaster@wforums.net>
2014-06-06 23:11:24 +0200

    Comments
    Added more comments. Only difftool needs to get comments now.

--- CCExtractorTester/Comparers/CompareData.cs
+++ CCExtractorTester/Comparers/CompareData.cs
@@ -2,12 +2,35 @@
 
 namespace CCExtractorTester
 {
+	/// <summary>
+	/// A class holding all the info that should be passed from the tester to the report generator.
+	/// </summary>
 	public class CompareData
 	{
+		/// <summary>
+		/// Gets or sets the correct file.
+		/// </summary>
+		/// <value>The correct file.</value>
 		public string CorrectFile { get; set; }
+		/// <summary>
+		/// Gets or sets the produced file.
+		/// </summary>
+		/// <value>The produced file.</value>
 		public string ProducedFile { get; set; }
+		/// <summary>
+		/// Gets or sets the sample file.
+		/// </summary>
+		/// <value>The sample file.</value>
 		public string SampleFile { get; set; }
+		/// <summary>
+		/// Gets or sets the commands used.
+		/// </summary>
+		/// <value>The command.</value>
 		public string Command { get; set; }
+		/// <summary>
+		/// Gets or sets the run time of the entry.
+		/// </summary>
+		/// <value>The run time.</value>
 		public TimeSpan RunTime { get; set; }
 	}
 }
--- CCExtractorTester/PerformanceLoggers/WindowsPerformanceCounters.cs
+++ CCExtractorTester/PerformanceLoggers/WindowsPerformanceCounters.cs
@@ -3,16 +3,39 @@ using System.Diagnostics;
 
 namespace CCExtractorTester
 {
+	/// <summary>
+	/// Windows performance counters.
+	/// </summary>
 	public class WindowsPerformanceCounters : IPerformanceLogger
 	{
+		/// <summary>
+		/// Gets or sets the logger.
+		/// </summary>
+		/// <value>The logger.</value>
 		private ILogger Logger { get; set; }
+		/// <summary>
+		/// Gets or sets the performance counter for RAM.
+		/// </summary>
+		/// <value>The ram.</value>
 		private PerformanceCounter Ram { get; set; }
+		/// <summary>
+		/// Gets or sets the performance counter for the cpu.
+		/// </summary>
+		/// <value>The cpu.</value>
 		private PerformanceCounter Cpu { get; set; }
+		/// <summary>
+		/// Gets or sets the process that's being monitored.
+		/// </summary>
+		/// <value>The proc.</value>
 		private Process Proc { get; set; }
 
 
 		#region IPerformanceLogger implementation
-
+		/// <summary>
+		/// Sets up the performance logger for a given process and using a given logger.
+		/// </summary>
+		/// <param name="logger">The logger to use for output.</param>
+		/// <param name="p">The process to monitor.</param>
 		public void SetUp (ILogger logger,Process p)
 		{
 			Logger = logger;
@@ -25,7 +48,9 @@ namespace CCExtractorTester
 				Logger.Error (e);
 			}
 		}
-
+		/// <summary>
+		/// Logs (in debug) the current value.
+		/// </summary>
 		public void DebugValue ()
 		{
 			if(Ram != null && Cpu != null){
@@ -35,8 +60,9 @@ namespace CCExtractorTester
 				}
 			}
 		}
-
-
+		/// <summary>
+		/// Logs (in debug) the stats.
+		/// </summary>
 		public void DebugStats ()
 		{
 			Logger.Debug(String.Format("Process data: handles opened: {0}, peak paged mem: {1}, peak virtual mem: {2}, peak mem: {3}, privileged cpu time: {4}, total cpu time: {5}",Proc.HandleCount,Proc.PeakPagedMemorySize64,Proc.PeakVirtualMemorySize64,Proc.PeakWorkingSet64,Proc.PrivilegedProcessorTime,Proc.TotalProcessorTime));
