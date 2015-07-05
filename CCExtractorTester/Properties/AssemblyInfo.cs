e761f388b701a16e6934e7d69e8dff8bc6820acd
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-07-05 04:35:05 +0200

    readme & assembly update
    Updated the git description

--- CCExtractorTester/Properties/AssemblyInfo.cs
+++ CCExtractorTester/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using CommandLine;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("0.5.*")]
+[assembly: AssemblyVersion ("0.7.*")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
@@ -24,4 +24,4 @@ using CommandLine;
 [assembly:AssemblyLicense(
 	"This is free software. You may redistribute copies of it under the terms of",
 	"the MIT License <http://www.opensource.org/licenses/mit-license.php>.")]
-[assembly:AssemblyUsage("Usage:","GUI: CCExtractorTester -g [-d]","Console: CCExtractorTester [-t test.xml] [-c confix.xml] [-d]")]
+[assembly:AssemblyUsage("Usage:","GUI: CCExtractorTester -g [-d]","Console: CCExtractorTester [-t test.xml] [-c config.xml] [-d]"),"Help: CCExtractortester --help"]
