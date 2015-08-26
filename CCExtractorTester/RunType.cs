bbf1b4485c137e7d3c65c3ff58aeb74af4f0c104
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-08-26 18:20:57 +0200

    Start of new Configuration manager
    The current configuration manager will be replaced by the new
    ConfigManager soon. This is a start for that.

---
+++ CCExtractorTester/RunType.cs
@@ -0,0 +1,21 @@
+﻿namespace CCExtractorTester
+{
+    /// <summary>
+    /// This enum defines how the test suite will behave during it's lifetime.
+    /// </summary>
+    enum RunType
+    {
+        /// <summary>
+        /// Generates reports and stores them on the executing machine.
+        /// </summary>
+        Report,
+        /// <summary>
+        /// Passes the results back to a server for further storage & processing.
+        /// </summary>
+        Server,
+        /// <summary>
+        /// Generates a matrix report for passed in folder using CCExtractor's built in report functionality.
+        /// </summary>
+        Matrix
+    }
+}
