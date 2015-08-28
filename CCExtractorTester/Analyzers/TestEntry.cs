5fb0c3161d4c1b331db56aa997bf3d38acbbf99b
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-08-28 16:45:35 +0200

    Test entry modifications
    Modifications to accomodate for the new xml format. Up to this commit,
    the old methods are still working (while the new ones are in progress)

--- CCExtractorTester/Analyzers/TestEntry.cs
+++ CCExtractorTester/Analyzers/TestEntry.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CCExtractorTester.Enums;
+using System;
+using System.Collections.Generic;
 
 namespace CCExtractorTester
 {
@@ -7,21 +9,62 @@ namespace CCExtractorTester
 	/// </summary>
 	public class TestEntry
 	{
-		public String TestFile { get; private set; }
-		public String Command { get; private set; }
-		public String ResultFile { get; private set; }
+        /// <summary>
+        /// Gets or sets the input file that will be used.
+        /// </summary>
+		public string InputFile { get; set; }
+
+        /// <summary>
+        /// Gets or sets the input type that will be used.
+        /// </summary>
+        public InputType InputFormat { get; set; }
+
+        /// <summary>
+        /// Gets or sets the command that will be passed to CCExtractor
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// Gets or sets the output format of the test entry.
+        /// </summary>
+        public OutputType OutputFormat { get; set; }
+        
+        /// <summary>
+        /// Gets or sets a list of files to compare
+        /// </summary>
+        public List<string> CompareFiles { get; set; }
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="CCExtractorTester.TestEntry"/> class.
+		/// Initializes a new instance of this class.
 		/// </summary>
-		/// <param name="testFile">The relative location (relative to SampleFolder configuration setting) of the file to test</param>
+		/// <param name="inputFile">The relative location (relative to SampleFolder configuration setting) of the file to test</param>
 		/// <param name="command">The command string to pass to CCExtractor.</param>
-		/// <param name="resultFile">The relative location (relative to CorrectResultFolder configuration setting) of the result file</param>
-		public TestEntry (string testFile, string command, string resultFile)
+		/// <param name="compareFile">The relative location (relative to CorrectResultFolder configuration setting) of the result file</param>
+		public TestEntry (string inputFile, string command, string compareFile)
 		{
-			TestFile = testFile;
+			InputFile = inputFile;
+            InputFormat = InputType.File;
 			Command = command;
-			ResultFile = resultFile;
+            OutputFormat = OutputType.File;
+            CompareFiles = new List<string>();
+            CompareFiles.Add(compareFile);
 		}
+
+        /// <summary>
+        /// Initializes a new instance of this class.
+        /// </summary>
+        /// <param name="inputFile">The relative location (relative to SampleFolder configuration setting) of the file to test</param>
+        /// <param name="inputFormat">The input type for CCExtractor</param>
+        /// <param name="command">The command string to pass to CCExtractor.</param>
+        /// <param name="outputFormat">The output type for CCExtractor</param>
+        /// <param name="compareFiles">The relative locations (relative to CorrectResultFolder configuration setting) of the result files</param>
+        public TestEntry(string inputFile, InputType inputFormat, string command, OutputType outputFormat, List<string> compareFiles)
+        {
+            InputFile = inputFile;
+            InputFormat = inputFormat;
+            Command = command;
+            OutputFormat = outputFormat;
+            CompareFiles = compareFiles;
+        }
 	}
 }
