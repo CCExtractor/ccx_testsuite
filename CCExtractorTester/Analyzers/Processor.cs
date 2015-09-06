1fce089c094e51821ea84de82cf68f3890e59619
Can I have some coffee? <git@canihavesome.coffee>
2015-09-06 19:07:32 +0200

    Added start of runner replacement
    -

---
+++ CCExtractorTester/Analyzers/Processor.cs
@@ -0,0 +1,9 @@
+﻿namespace CCExtractorTester.Analyzers
+{
+    /// <summary>
+    /// This class calls CCExtractor with an input file (or other method), a set of parameters and output files.
+    /// </summary>
+    class Processor
+    {
+    }
+}
