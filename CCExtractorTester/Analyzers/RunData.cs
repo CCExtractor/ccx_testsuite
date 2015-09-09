3ce4a25bf3b3e4aed47725529c30fb400a2f2d30
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2015-09-09 23:04:27 +0200

    Updates to processor
    -

--- CCExtractorTester/Analyzers/RunData.cs
+++ CCExtractorTester/Analyzers/RunData.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 namespace CCExtractorTester.Analyzers
 {
 	public class RunData {
 		public int ExitCode { get; set; }
 		public TimeSpan Runtime { get; set; }
+
+        public Dictionary<string,string> ResultFiles { get; set; }
 	}
 }
