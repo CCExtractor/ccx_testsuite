012b1b41ee100d5bdbb477e3de717a417aa80a18
wforums <webmaster@wforums.net>
2015-10-07 20:09:09 +0200

    updates hashing algorithm used
    now has a generic hash function, and uses sha256 instead of md5 for base
    comparisons

--- CCExtractorTester/Comparers/Hasher.cs
+++ CCExtractorTester/Comparers/Hasher.cs
@@ -10,20 +10,25 @@ namespace CCExtractorTester.Comparers
 	public static class Hasher
 	{
         /// <summary>
-        /// Gets the MD5 hash of a given file.
+        /// Gets the hash for a given file and given algorithm.
         /// </summary>
-        /// <param name="fileLocation">The location of the file to generate a hash for</param>
-        /// <returns>A string wit the MD5 hash, or empty if the file does not exist.</returns>
-		public static String getFileMD5Hash(String fileLocation){
-			if (File.Exists (fileLocation)) {
-				using (var md5 = MD5.Create ()) {
-					using (var stream = File.OpenRead (fileLocation)) {
-						return BitConverter.ToString (md5.ComputeHash (stream)).Replace ("-", "").ToLower ();
-					}
-				}
-			}
-			return "";
-		}
+        /// <param name="fileLocation">The location of the file to generate a hash for.</param>
+        /// <param name="hashAlgorithm">The hash algorithm to use. Defaults to SHA256.</param>
+        /// <returns></returns>
+        public static string getFileHash(String fileLocation, String hashAlgorithm = "SHA256")
+        {
+            if (File.Exists(fileLocation))
+            {
+                using (var stream = File.OpenRead(fileLocation))
+                {
+                    using (var algo = HashAlgorithm.Create(hashAlgorithm))
+                    {
+                        return BitConverter.ToString(algo.ComputeHash(stream)).Replace("-", "").ToLower();
+                    }
+                }
+            }
+            return String.Empty;
+        }
 
         /// <summary>
         /// Checks if two given files are equal.
@@ -32,8 +37,8 @@ namespace CCExtractorTester.Comparers
         /// <param name="file2">The second file.</param>
         /// <returns>True if they are equal, false otherwise.</returns>
 		public static bool filesAreEqual(String file1, String file2){
-			String first = getFileMD5Hash (file1);
-			String second = getFileMD5Hash (file2);
+			String first = getFileHash(file1);
+			String second = getFileHash(file2);
 			return first.CompareTo (second) == 0;				
 		}
 	}
